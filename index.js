205e9c1e7ea0f4475a307be8d7b124f3de9d4490
mdnriyas17 <mdnriyas@gmail.com>
2023-09-12 20:26:52 +0530

    add

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-`mongoose.connect(process.env.MONGODB_URI)`
+mongoose.connect(process.env.MONGODB_URI)
 
 app.get(
     "/getUsers", (async (req, res) => {
